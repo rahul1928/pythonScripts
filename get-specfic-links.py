37cf092c1f5195b01594f993bcaf4bbd092b337b
rahul1928 <dubey.rahul0055@gmail.com>
2016-06-02 17:14:51 +0530

    save dict to json

--- get-specfic-links.py
+++ get-specfic-links.py
@@ -2,16 +2,28 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
 import re
-
 import sys
+import json
+
 
 url = str(sys.argv[1])
 print url
-html_page = urllib2.urlopen(url)
-soup = BeautifulSoup(html_page)
-div = soup.find('div', id='srchRslt')
-for link in div.findAll('a'):
-    name = (link.string)
-    href = (link.get('href'))
-    print str(name) + ' : ' + 'https://www.1mg.com' + str(href) 
+
+mydict = {}
+
+def scrap(url):
+    html_page = urllib2.urlopen(url)
+    soup = BeautifulSoup(html_page)
+    div = soup.find('div', id='srchRslt')
+    for link in div.findAll('a'):
+         name = (link.string)
+         href = (link.get('href'))
+         href = 'https://www.1mg.com' + str(href)
+         mydict[str(name)] = str(href)        
+
+scrap(url)
+
+
+data = json.dumps([{'name': key, 'url': value} for key,value in mydict.items()], indent=4)
+print(data)
 
